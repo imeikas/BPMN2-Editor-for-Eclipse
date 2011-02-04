ae9b8bd9f81756e2890e2d3b595baa68157eb970
Ivar Meikas <imeikas@gmail.com>
2011-02-04 16:52:31 +0200

    Disabled Model View

--- org.jboss.bpmn2.editor.ui/src/org/jboss/bpmn2/editor/ui/preferences/PreferenceConstants.java
+++ org.jboss.bpmn2.editor.ui/src/org/jboss/bpmn2/editor/ui/preferences/PreferenceConstants.java
@@ -4,13 +4,4 @@ package org.jboss.bpmn2.editor.ui.preferences;
  * Constant definitions for plug-in preferences
  */
 public class PreferenceConstants {
-
-	public static final String P_PATH = "pathPreference";
-
-	public static final String P_BOOLEAN = "booleanPreference";
-
-	public static final String P_CHOICE = "choicePreference";
-
-	public static final String P_STRING = "stringPreference";
-	
 }
