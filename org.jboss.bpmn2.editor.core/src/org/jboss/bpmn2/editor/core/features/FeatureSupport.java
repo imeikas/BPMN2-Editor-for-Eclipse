9f1043302587d2953f93944f1334ee80cc8021f7
Innar Made <imade@codehoop.com>
2011-02-09 14:16:28 +0200

    Reimplemented lane to lane with custom redraw logic. This redraw could be used with other lane movements too (TODO).

--- org.jboss.bpmn2.editor.core/src/org/jboss/bpmn2/editor/core/features/FeatureSupport.java
+++ org.jboss.bpmn2.editor.core/src/org/jboss/bpmn2/editor/core/features/FeatureSupport.java
@@ -1,11 +1,15 @@
 package org.jboss.bpmn2.editor.core.features;
 
+import java.awt.Dimension;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.bpmn2.Lane;
 import org.eclipse.graphiti.datatypes.ILocation;
 import org.eclipse.graphiti.features.context.ITargetContext;
 import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
+import org.eclipse.graphiti.mm.algorithms.Text;
 import org.eclipse.graphiti.mm.pictograms.ContainerShape;
 import org.eclipse.graphiti.mm.pictograms.Diagram;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
@@ -106,5 +110,121 @@ public abstract class FeatureSupport {
 		}
 
 		packLanes(container.getContainer());
+	}
+
+	public void redraw(ContainerShape container) {
+		ContainerShape root = getRootContainer(container);
+		resizeRecursively(root);
+		postResizeFixLenghts(root);
+	}
+
+	private ContainerShape getRootContainer(ContainerShape container) {
+		ContainerShape parent = container.getContainer();
+		Object bo = getBusinessObject(parent);
+		if (bo != null && bo instanceof Lane) {
+			return getRootContainer(parent);
+		}
+		return container;
+	}
+
+	private Dimension resize(ContainerShape container) {
+		Lane lane = (Lane) getBusinessObject(container);
+		IGaService service = Graphiti.getGaService();
+		int height = 0;
+		int width = container.getGraphicsAlgorithm().getWidth() - 15;
+		List<GraphicsAlgorithm> gaList = new ArrayList<GraphicsAlgorithm>();
+		
+		for (Shape s : container.getChildren()) {
+			Object bo = getBusinessObject(s);
+			if (bo != null && bo instanceof Lane && !bo.equals(lane)) {
+				GraphicsAlgorithm ga = s.getGraphicsAlgorithm();
+				service.setLocation(ga, 15, height);
+				height += ga.getHeight() - 1;
+				if (ga.getWidth() >= width) {
+					width = ga.getWidth();
+				} else {
+					service.setSize(ga, width, ga.getHeight());
+				}
+				gaList.add(ga);
+			}
+		}
+		
+		GraphicsAlgorithm ga = container.getGraphicsAlgorithm();
+
+		if (height == 0) {
+			return new Dimension(ga.getWidth(), ga.getHeight());
+		} else {
+			int newWidth = width + 15;
+			int newHeight = height + 1;
+			service.setSize(ga, newWidth, newHeight);
+			
+			for (Shape s : container.getChildren()) {
+				if (s.getGraphicsAlgorithm() instanceof Text) {
+					s.getGraphicsAlgorithm().setHeight(newHeight);
+				}
+			}
+			
+			return new Dimension(newWidth, newHeight);
+		}
+	}
+
+	private Dimension resizeRecursively(ContainerShape root) {
+		Lane lane = (Lane) getBusinessObject(root);
+		List<Dimension> dimensions = new ArrayList<Dimension>();
+		int foundLanes = 0;
+
+		for (Shape s : root.getChildren()) {
+			Object bo = getBusinessObject(s);
+			if (s instanceof ContainerShape && bo != null && bo instanceof Lane && !bo.equals(lane)) {
+				foundLanes += 1;
+				Dimension d = resizeRecursively((ContainerShape) s);
+				if (d != null) {
+					dimensions.add(d);
+				}
+			}
+		}
+		
+		if(dimensions.isEmpty()) {
+			GraphicsAlgorithm ga = root.getGraphicsAlgorithm();
+			return new Dimension(ga.getWidth(), ga.getHeight());
+		}
+		
+		if (foundLanes > 0) {
+			return resize(root);
+		}
+
+		return getMaxDimension(dimensions);
+	}
+
+	private Dimension getMaxDimension(List<Dimension> dimensions) {
+		if (dimensions.isEmpty()) {
+			return null;
+		}
+		int height = 0;
+		int width = 0;
+
+		for (Dimension d : dimensions) {
+			height += d.height;
+			if (d.width > width) {
+				width = d.width;
+			}
+		}
+
+		return new Dimension(width, height);
+	}
+	
+	private void postResizeFixLenghts(ContainerShape root) {
+		IGaService service = Graphiti.getGaService();
+		Lane lane = (Lane) getBusinessObject(root);
+		int width = root.getGraphicsAlgorithm().getWidth() - 15;
+		
+		for(Shape s : root.getChildren()) {
+			Object o = getBusinessObject(s);
+			if(s instanceof ContainerShape && o != null && o instanceof Lane && !o.equals(lane)) {
+				GraphicsAlgorithm ga = s.getGraphicsAlgorithm();
+				service.setSize(ga, width, ga.getHeight());
+				postResizeFixLenghts((ContainerShape) s);
+			}
+		}
 	}
 }
--- org.jboss.bpmn2.editor.core/src/org/jboss/bpmn2/editor/core/features/lane/MoveFromLaneToLaneFeature.java
+++ org.jboss.bpmn2.editor.core/src/org/jboss/bpmn2/editor/core/features/lane/MoveFromLaneToLaneFeature.java
@@ -4,6 +4,7 @@ import org.eclipse.bpmn2.Lane;
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.context.IMoveShapeContext;
 import org.eclipse.graphiti.mm.pictograms.ContainerShape;
+import org.jboss.bpmn2.editor.core.ModelHandler;
 
 public class MoveFromLaneToLaneFeature extends MoveLaneFeature {
 
@@ -32,11 +33,18 @@ public class MoveFromLaneToLaneFeature extends MoveLaneFeature {
 	protected void internalMove(IMoveShapeContext context) {
 		Lane movedLane = getMovedLane(context);
 		Lane targetLane = getTargetLane(context);
-		modifyModelStructure();
-		int numberOfLanes = targetLane.getChildLaneSet().getLanes().size();
+		Lane sourceLane = getSourceLane(context);
+		modifyModelStructure(sourceLane, targetLane, movedLane);
+		support.redraw(context.getSourceContainer());
 	}
 	
-	private void modifyModelStructure() {
+	private void modifyModelStructure(Lane sourceLane, Lane targetLane, Lane movedLane) {
+		sourceLane.getChildLaneSet().getLanes().remove(movedLane);
+		
+		if(targetLane.getChildLaneSet() == null) {
+			targetLane.setChildLaneSet(ModelHandler.FACTORY.createLaneSet());
+		}
+		targetLane.getChildLaneSet().getLanes().add(movedLane);
     }
 
 	private Lane getTargetLane(IMoveShapeContext context) {
