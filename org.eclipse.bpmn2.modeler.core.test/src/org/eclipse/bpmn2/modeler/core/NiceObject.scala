5d9ca32002e5403a66d4e8b22617e10b67701584
Ivar Meikas <imeikas@gmail.com>
2011-06-21 12:47:52 +0300

    Test were to outdated.

--- org.eclipse.bpmn2.modeler.core.test/src/org/eclipse/bpmn2/modeler/core/NiceObject.scala
+++
@@ -1,9 +0,0 @@
-package org.eclipse.bpmn2.modeler.core
-
-class NiceObject[T <: AnyRef](x: T) {
-  def niceClass: Class[_ <: T] = x.getClass.asInstanceOf[Class[T]]
-}
-
-object NiceObject{
-	  implicit def toNiceObject[T <: AnyRef](x: T) = new NiceObject(x)
-}
